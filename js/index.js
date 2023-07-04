62995355e893ac65b10f81019a88eee13f4345d4
TIENANH\ASUS <newbiekas1312@gmail.com>
2023-07-04 10:21:48 +0700

    index

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
+const dropdownLink = document.querySelector('.dropdown-link');
+const dropdownText = document.querySelector('.dropdown-text');
+const educationColumns = document.querySelectorAll('.education-column');
+const overlay = document.querySelector('.overlay');
+
+dropdownLink.addEventListener('click', function() {
+  educationColumns.forEach(function(column) {
+    column.classList.toggle('expanded');
+  });
+  
+  dropdownText.textContent = educationColumns[0].classList.contains('expanded') ? 'Thu gọn' : 'Tất cả nghành học';
+  dropdownLink.querySelector('i').classList.toggle('bi-chevron-down');
+  dropdownLink.querySelector('i').classList.toggle('bi-chevron-up');
+  overlay.classList.toggle('active');
+
+  if (!educationColumns[0].classList.contains('expanded')) {
+    const allCourses = document.querySelectorAll('#educationList li');
+    allCourses.forEach(function(course, index) {
+      setTimeout(function() {
+        course.style.transform = 'translateY(0)';
+        course.style.opacity = '1';
+      }, index * 100);
+    });
+  }
+  
+});
 
