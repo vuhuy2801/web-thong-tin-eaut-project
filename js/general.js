953413aaa778597d0e46419aa9dfa727eaa3841f
TIENANH\ASUS <newbiekas1312@gmail.com>
2023-07-15 17:28:48 +0700

    update header

--- js/general.js
+++ js/general.js
@@ -1,43 +1,12 @@
 (function() {
     "use strict";
   
-    // đang có bug
-    // Thanh NAV Khi Cuộn
-    // var prevScrollpos = window.pageYOffset;
-    // window.addEventListener("scroll", function () {
-    //   var currentScrollPos = window.pageYOffset;
-    //   var header = document.getElementById("header");
-    //   if (prevScrollpos > currentScrollPos) {
-    //     header.style.transform = "translateY(0)";
-    //     header.style.opacity = "1";
-    //   } else {
-    //     header.style.transform = "translateY(-100%)";
-    //     header.style.opacity = "0";
-    //   }
-    //   prevScrollpos = currentScrollPos;
-    // });
-  // End Thanh NAV Khi Cuộn
-  
-
-  
-  
-
   // Moblie nav
   $('.mobile-nav-toggle').on('click', function(e) {
     $('#navbar').toggleClass('navbar-mobile');
     $(this).toggleClass('bi-list bi-x');
     $('#sub-navbar').toggleClass('active');
   });
-  
-  
-
-  // để sau này thêm chưa biết thêm gì
-  // var hr = document.querySelector(' ');
-  // var navbarMobile = document.querySelector('.navbar-mobile');
-  // navbarMobile.appendChild(hr);
-  
-  // bỏ thanh ngang khi lướt ngang
-
   
   let isScrolling = false;
 
