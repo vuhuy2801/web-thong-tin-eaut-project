8806d7e471d16718543503b4f50d345d41e44a7f
VUHUY\ADMIN <guguyb1@gmail.com>
2023-07-21 01:40:47 +0700

    add render api post

--- js/new.js
+++ js/new.js
@@ -1,3 +1,52 @@
+let elmSinglePost = document.querySelector(".single-post");
+
+const VALUE_SEARCH_PARAMS = new URLSearchParams(window.location.search);
+const ARTICLES_ID = VALUE_SEARCH_PARAMS.get("slug");
+const URL_DETAILS = `bai-viets?populate=*&filters[$and][0][slug][$eq]=${ARTICLES_ID}`;
+
+
+
+const API_NEWS = axios.create({
+  baseURL: "http://api.vuhuy.xyz/api/"
+ 
+});
+
+
+
+function getPostContent() {
+  API_NEWS.get(URL_DETAILS)
+    .then((response) => {
+      if(response.data.data == ""){
+       return;
+      }
+      renderPostContent(response.data.data[0].attributes);
+      // elmHeadTitle.innerHTML = response.data.data.title;
+      // renderPostContent(response.data.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    
+    });
+}
+
+
+function renderPostContent(data) {
+
+  let str = `
+  <div class="post-meta">
+  <span class="date">EAUT-CNTT</span>
+  <span class="mx-1">&bullet;</span>
+  <span>Thứ Sáu, 21/10/2022</span>
+</div>
+ <h1 class="mb-5">${data.title}
+ </h1>
+ ${data.content}
+  `
+  
+  elmSinglePost.innerHTML = str;
+}
+
+getPostContent();
 
 var navContainer = document.getElementById('pills-tab');
 navContainer.addEventListener('click', function(event) {
--- js/news.js
+++ js/news.js
@@ -1,13 +1,195 @@
+let elmPagination = document.querySelector("#pagination");
+let elmBtnNext = document.querySelector("#btn-next");
+let elmBtnPrev = document.querySelector("#btn-prev");
+let elmPostList = document.querySelector("#postList");
+let lastPage;
+const REGEX = /^[1-9]\d*$/;
 
-document.addEventListener("DOMContentLoaded", function () {
-    var pageItems = document.querySelectorAll(".page-item");
-    for (var i = 0; i < pageItems.length; i++) {
-      pageItems[i].addEventListener("click", function () {
-        for (var j = 0; j < pageItems.length; j++) {
-          pageItems[j].classList.remove("active");
-        }
-        this.classList.add("active");
-      });
+const CURRENT_URL = new URL(window.location);
+const VALUE_SEARCH_PARAMS = new URLSearchParams(window.location.search);
+const CATE_ID = parseInt(VALUE_SEARCH_PARAMS.get("id"));
+const PAGES = parseInt(VALUE_SEARCH_PARAMS.get("page"));
+
+let currentPage = PAGES;
+
+const API_NEWS = axios.create({
+  baseURL: "http://api.vuhuy.xyz/api/",
+});
+
+if (!isValidId() || !isVaildPage()) {
+  // Nếu giá trị không hợp lệ, điều hướng URL về trang index.html
+  window.location.href = "/index.html";
+}
+
+function isValidId() {
+  return REGEX.test(CATE_ID);
+}
+
+function isVaildPage() {
+  return REGEX.test(PAGES);
+}
+
+//event next & previous pages
+elmBtnNext.addEventListener("click", function () {
+  if (currentPage === lastPage) {
+    return;
+  }
+  currentPage++;
+  getPaginationForNews(currentPage);
+  CURRENT_URL.searchParams.set("page", currentPage);
+  window.history.pushState({}, "", CURRENT_URL);
+});
+elmBtnPrev.addEventListener("click", function () {
+  if (currentPage === 1) {
+    return;
+  }
+  currentPage--;
+  getPaginationForNews(currentPage);
+  CURRENT_URL.searchParams.set("page", currentPage);
+  window.history.pushState({}, "", CURRENT_URL);
+});
+
+elmPagination.addEventListener("click", function (e) {
+  if (e.target.tagName === "SPAN") {
+    if (currentPage === parseInt(e.target.getAttribute("data-page"))) {
+      return;
     }
-  });
-  
+    currentPage = parseInt(e.target.getAttribute("data-page"));
+    getPaginationForNews(currentPage);
+    CURRENT_URL.searchParams.set("page", currentPage);
+    window.history.pushState({}, "", CURRENT_URL);
+  }
+});
+
+function getPaginationForNews(page) {
+  return API_NEWS.get("bai-viets", {
+    params: {
+      pagination: {
+        page: page,
+        pageSize: 6,
+      },
+      filters: {
+        $and: [
+          {
+            danh_muc: {
+              id: {
+                $eq: CATE_ID,
+              },
+            },
+          },
+        ],
+      },
+      populate: {
+        headerImage: {
+          fields: ["formats"],
+        },
+      },
+      fields: ["title", "description", "slug", "createdAt"],
+    },
+  })
+    .then((response) => {
+      console.log(response.data);
+      renderPostNews(response.data.data);
+      lastPage = response.data.meta.pagination.pageCount;
+      renderPaginationButton(page);
+      statusButton();
+      // return response;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function renderPostNews(data) {
+  let str = "";
+
+  for (let i = 0; i < data.length; i++) {
+    dayjs.locale("vi");
+    const CUSTOM_DATE = dayjs(data[i].attributes.createdAt);
+    let date = CUSTOM_DATE.format("dddd, DD/MM/YYYY");
+
+    str += ` 
+    <div class="col-lg-4">
+      <div class="post-box">
+        <div class="post-img">
+        <a href="new.html?slug=${data[i].attributes.slug}">
+          <img
+            src="http://api.vuhuy.xyz${data[i].attributes.headerImage.data.attributes.formats.small.url}"
+            class="img-fluid"
+            alt="${data[i].attributes.headerImage.data.attributes.formats.small.name}"
+          />
+          </a>
+        </div>
+        <p class="post-date">${date}</p>
+
+        <h3 class="post-title">
+          <a href="new.html?slug=${data[i].attributes.slug}">${data[i].attributes.title}</a>
+        </h3>
+        <p class="description">
+        ${data[i].attributes.description}
+        </p>
+      </div>
+  </div>`; // render articles
+  }
+  elmPostList.innerHTML = str;
+}
+
+function renderPaginationButton(indexPage) {
+  currentPage = indexPage;
+  const numPagesToShow = 5;
+  let str = "";
+
+  const generatePageItem = (index, active = false) => {
+    return `
+      <li class="page-item ${active ? "active" : ""}">
+        <span class="page-link" data-page="${index}">${index}</span>
+      </li>`;
+  };
+
+  if (lastPage <= numPagesToShow) {
+    for (let i = 1; i <= lastPage; i++) {
+      str += generatePageItem(i, i === indexPage);
+    }
+  } else {
+    if (indexPage > numPagesToShow - 2) {
+      str += generatePageItem(1);
+      if (indexPage > numPagesToShow - 1) {
+        str += `<li class="page-item"><a class="page-link">..</a></li>`;
+      }
+    }
+
+    const startIndex = Math.max(indexPage - 2, 1);
+    const endIndex = Math.min(startIndex + numPagesToShow - 1, lastPage);
+
+    for (let i = startIndex; i <= endIndex; i++) {
+      str += generatePageItem(i, i === indexPage);
+    }
+
+    if (lastPage - indexPage > numPagesToShow - 2) {
+      if (lastPage - indexPage > numPagesToShow - 1) {
+        str += `<li class="page-item"><a class="page-link">..</a></li>`;
+      }
+      str += generatePageItem(lastPage);
+    }
+  }
+
+  elmPagination.innerHTML = str;
+}
+
+function statusButton() {
+  if (currentPage > 1) {
+    elmBtnPrev.classList.remove("disabled");
+  } else {
+    elmBtnPrev.classList.add("disabled");
+  }
+
+  if (currentPage == lastPage) {
+    elmBtnNext.classList.add("disabled");
+  } else {
+    elmBtnNext.classList.remove("disabled");
+  }
+}
+
+
+
+getPaginationForNews(PAGES);
